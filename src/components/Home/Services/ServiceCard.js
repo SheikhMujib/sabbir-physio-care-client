e402e9a400dfe22652175c99f90b15eb8640f520
SheikhMujib <mbasheikhmujib@gmail.com>
2022-11-15 21:31:37 +0600

    Service description added

--- src/components/Home/Services/ServiceCard.js
+++ src/components/Home/Services/ServiceCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ServiceCard = ({ service }) => {
-  const { _id, img, title, price } = service;
+  const { img, title, price, description } = service;
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
       <figure>
@@ -12,8 +12,9 @@ const ServiceCard = ({ service }) => {
         <p className="text-2xl font-semibold text-orange-600">
           Price: ${price}
         </p>
+        <p>{description.slice(0, 100)}...</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-primary">View Details</button>
         </div>
       </div>
     </div>
